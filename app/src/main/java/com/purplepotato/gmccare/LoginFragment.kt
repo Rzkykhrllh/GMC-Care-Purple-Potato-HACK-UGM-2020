ddb3faf8f99d9f1ee18c4368d680f273b166d8bb
mfathur <mfathur17@gmail.com>
2020-09-28 23:44:09 +0700

    edit sign up & login layout

--- app/src/main/java/com/purplepotato/gmccare/LoginFragment.kt
+++ app/src/main/java/com/purplepotato/gmccare/LoginFragment.kt
@@ -16,7 +16,6 @@ import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment(), View.OnClickListener {
 
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
